5b55135762e8f73c7f54eb8f29ce024f07c651ec
Adarsh Danda <adarshdanda@wirelessprv-10-195-155-51.near.illinois.edu>
2024-09-29 03:07:50 -0500

    added HomePage and NavBarComponent

---
+++ Project/frontend/src/components/HomePageComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function HomePageBodyComponent() {
+    return (<>
+        <div>
+            <img>Overlay text and button over this img</img>
+            <p>RecMusic</p>
+            <p>Give some info about how the machine learning model works</p>
+            <button>Find recommendations</button>
+        </div>
+    </>)
+}
+
+export default HomePageBodyComponent;
---
+++ Project/frontend/src/components/NavBarComponent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function NavBarComponent() {
+    return (
+    <>
+        <nav>
+            <img></img>
+            <ul>
+                <li>Home</li>
+                <li>Input Music</li>
+                <li>About</li>
+            </ul>
+        </nav>
+    </>)
+}
+
+export default NavBarComponent;
