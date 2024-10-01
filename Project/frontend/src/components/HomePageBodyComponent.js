9aa4516f546b249b89a81239a86a5ba68797a698
Adarsh Danda <adarshdanda@wirelessprv-10-195-155-51.near.illinois.edu>
2024-10-01 10:21:37 -0500

    fixed navbar alignment

--- Project/frontend/src/components/HomePageBodyComponent.js
+++ Project/frontend/src/components/HomePageBodyComponent.js
@@ -6,17 +6,17 @@ function HomePageBodyComponent() {
         <div class="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
             <div class="relative">
                 <div class="lg:w-2/3">
-                    <p class="text-sm font-normal tracking-widest text-gray-300 uppercase">Getting Better Music Recommendations</p>
-                    <h1 class="mt-6 text-4xl font-normal text-white sm:mt-10 sm:text-5xl lg:text-6xl xl:text-8xl"><span class="text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-red-700">Perfect Your</span> Playlists</h1>
+                    <p class="max-w-lg text-sm font-normal tracking-wider text-gray-300 uppercase">Getting Better Music Recommendations</p>
+                    <h1 class="max-w-lg mt-6 text-4xl font-normal text-white sm:mt-10 sm:text-5xl lg:text-6xl xl:text-8xl"><span class="text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-red-700">Perfect Your</span> Playlists</h1>
                     <p class="max-w-lg mt-4 text-xl font-normal text-gray-400 sm:mt-8">Our machine learning models analyzes many various features of songs including tempo, genre, and lyrics. Input any song or playlist into our model and we'll give you recommendations. Add more info about how model works.</p>
-                    <div class="relative inline-flex items-center justify-center mt-8 sm:mt-12 group">
+                    <div class="mr-72 relative inline-flex items-center justify-center mt-8 sm:mt-12 group">
                         <div class="absolute transition-all duration-200 rounded-full -inset-px bg-gradient-to-r from-red-500 to-pink-800 group-hover:shadow-lg group-hover:shadow-red-500/70"></div>
                         <a href="#" title="" class="relative inline-flex items-center justify-center px-8 py-3 text-base font-normal text-white bg-black border border-transparent rounded-full" role="button"> Get Started With Music </a>
                     </div>
 
                     <div>
-                        <div class="inline-flex items-center pt-6 mt-8 border-t border-gray-800 sm:pt-10 sm:mt-14">
-                            <span class="ml-2 text-base font-normal text-white"> Over 10,000+ songs recommended to users or some other value</span>
+                        <div class="mr-72 pr-4 inline-flex items-center pt-6 mt-8 border-t border-gray-800 sm:pt-10 sm:mt-14">
+                            <span class="ml-2 text-base font-normal text-gray-400"> Over 10,000+ songs recommended to users or some other value</span>
                         </div>
                     </div>
                 </div>
--- Project/frontend/src/components/NavBarComponent.js
+++ Project/frontend/src/components/NavBarComponent.js
@@ -5,14 +5,14 @@ import logoImg from './img/MusicLogo.jpg'
 function NavBarComponent() {
     return (
     <>
-        <header className="fpy-4 bg-black sm:py-6 h-25">
-            <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8 flex flex-row justify-between">
+        <header className="fpy-4 pt-4 bg-black h-25">
+            <div className="px-36 ml-1 mr-1 mx-auto max-w-full h-full flex flex-row justify-between">
                 <div className="flex items-center">
                     <img className="w-auto h-20" src={logoImg} alt="Img not found"/>
                 </div>
 
                 <nav>
-                    <div className="pt-8 pb-4 space-x-10">
+                    <div className="pt-7 pb-7 space-x-10 h-full">
                         <a href="#" title="" className="text-base font-normal text-gray-400 transition-all duration-200 hover:text-white"> Home </a>
 
                         <a href="#" title="" className="text-base font-normal text-gray-400 transition-all duration-200 hover:text-white"> Music Recommendations </a>
