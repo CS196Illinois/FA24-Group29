5308429b385e2f25bbb428ee26ebfda5860dcb78
Adarsh Danda <adarshdanda@wirelessprv-10-195-155-51.near.illinois.edu>
2024-10-01 01:46:37 -0500

    implemented nav bar without any links

--- Project/frontend/src/components/NavBarComponent.js
+++ Project/frontend/src/components/NavBarComponent.js
@@ -1,19 +1,27 @@
 import React from 'react';
+import logoImg from './img/MusicLogo.jpg'
+
 
 function NavBarComponent() {
     return (
     <>
-        <nav className="flex flex-row bg-white border-yellow-300 border-2 rounded-b-xl h-14 justify-between">
-            <img className="border-collapse"></img>
-            <div className='basis-1/3 -space-x-36'>
-                <ul className="grid grid-cols-3 bg-slate-500 text-center h-full">
-                    <a className="border-2" href="#">Home</a>
-                    <a className="border-2" href="#">Input Music</a>
-                    <a className="border-2" href="#">About</a>
-                </ul>
-            </div>
+        <header className="fpy-4 bg-black sm:py-6 h-25">
+            <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8 flex flex-row justify-between">
+                <div className="flex items-center">
+                    <img className="w-auto h-20" src={logoImg} alt="Img not found"/>
+                </div>
 
-        </nav>
+                <nav>
+                    <div className="pt-8 pb-4 space-x-10">
+                        <a href="#" title="" className="text-base font-normal text-gray-400 transition-all duration-200 hover:text-white"> Home </a>
+
+                        <a href="#" title="" className="text-base font-normal text-gray-400 transition-all duration-200 hover:text-white"> Input Music </a>
+
+                        <a href="#" title="" className="text-base font-normal text-gray-400 transition-all duration-200 hover:text-white"> About </a>
+                    </div>
+                </nav>
+            </div>
+        </header>
     </>)
 }
 
