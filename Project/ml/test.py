d1aab5c0ddfc1a38c91fbfd1ec95a6b599f93f3a
hallucinateERROR <73016325+hallucinateERROR@users.noreply.github.com>
2024-10-09 17:55:31 -0500

    finished model

--- Project/ml/test.py
+++ Project/ml/test.py
@@ -67,11 +67,26 @@ projection['cluster'] = df['cluster_label']
 fig = px.scatter(
     projection, x='x', y='y', color='cluster', hover_data=['x', 'y', 'title']
 )
-#fig.show()
+fig.show()
 ## end of exploratory data analysis
 
 
 ##building the song recommendation program
+
+#reformating input from an array of dictionaries to a dictionary of a list
+def flatten_dict_list(dict_list):
+
+    flattened_dict = defaultdict()
+
+    for key in dict_list[0].keys():
+        flattened_dict[key] = []
+    
+    for dictionary in dict_list:
+        for key, value in dictionary.items():
+            flattened_dict[key].append(value)
+
+    return flattened_dict
+
 #retrieve information of searched song
 def get_song_data(name, year):
     #TO_DO - replace == with .contains
@@ -99,15 +114,37 @@ def get_mean_vector(song_list):
     print(song_matrix)
     return np.mean(song_matrix, axis=0)
 
-#def recommend_songs(song_list, n_songs=10):
+
+
+
+#actual recommendation function
+def recommend_songs(song_list, n_songs=10):
     #metadata metrics tbd
-#    metadata_cols = ['name', 'year', 'artists']
+    metadata_cols = ['track_name', 'release_date', 'artist_name']
+    song_dict = flatten_dict_list(song_list)
 
+    song_center = get_mean_vector(song_list)
+    scaler = song_cluster_pipeline[0]
 
+    #scaling xy for measuring distance (similarity)
+    scaled_df = scaler.transform(df[number_cols])
+    scaled_song_center = scaler.transform(song_center)
+    distance = cdist(scaled_song_center, scaled_df, 'cosine')
+    print(distance)
+    index = list(np.argsort(distance)[:, :n_songs][0])
 
+    rec_songs = df.iloc[index]
+    #TO_DO: modify such that after deducting the input songs, the there are no less that 10 songs given
+    rec_songs = rec_songs[~rec_songs['track_name'].isin(song_dict['name'])]
+    formatted_rec = rec_songs[metadata_cols].to_dict(orient="records")
+    return formatted_rec
 
-mean_vector = get_mean_vector([{'name': 'The Story of Us', 'year': 2010},
+#main
+def main():
+    rec_songs = recommend_songs([{'name': 'The Story of Us', 'year': 2010},
                 {'name': 'Natalie', 'year': 2012},
                 {'name': 'All Apologies', 'year': 1993},
                 {'name': 'Stay Away', 'year': 1993}])
-print(mean_vector)
+    print(rec_songs)
+
+main()
