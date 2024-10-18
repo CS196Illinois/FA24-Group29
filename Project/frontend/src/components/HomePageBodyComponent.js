9f2841d1d4e538ce0925baa97ffe4ed05842c97e
Omar Shatat <omarsshatat06@gmail.com>
2024-10-18 13:24:01 -0500

    new background changed colors

--- Project/frontend/src/components/HomePageBodyComponent.js
+++ Project/frontend/src/components/HomePageBodyComponent.js
@@ -3,15 +3,15 @@ import {Link} from 'react-router-dom';
 
 function HomePageBodyComponent() {
     return (<>
-    <section class="py-12 bg-black sm:pb-16 lg:pb-20 xl:pb-24">
+    <section class="py-12 bg-[#0C1821] sm:pb-16 lg:pb-20 xl:pb-24">
         <div class="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl pt-20">
             <div class="relative">
                 <div class="lg:w-2/3">
                     <p class="max-w-lg text-sm font-normal tracking-wider text-gray-300 uppercase">Getting Better Music Recommendations</p>
-                    <h1 class="max-w-lg mt-6 text-4xl font-normal text-white sm:mt-10 sm:text-5xl lg:text-6xl xl:text-8xl"><span class="text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-red-700">Perfect Your</span> Playlists</h1>
+                    <h1 class="max-w-lg mt-6 text-4xl font-normal text-white sm:mt-10 sm:text-5xl lg:text-6xl xl:text-8xl"><span class="text-transparent text-[#324A5F]">Perfect Your</span> Playlists</h1>
                     <p class="max-w-lg mt-4 text-xl font-normal text-gray-400 sm:mt-8">Our machine learning models analyzes many various features of songs including tempo, genre, and lyrics. Input any song or playlist into our model and we'll give you recommendations. Add more info about how model works.</p>
                     <div class="mr-72 relative inline-flex items-center justify-center mt-8 sm:mt-12 group">
-                        <div class="absolute transition-all duration-200 rounded-full -inset-px bg-gradient-to-r from-red-500 to-pink-800 group-hover:shadow-lg group-hover:shadow-red-500/70"></div>
+                        <div class="absolute transition-all duration-200 rounded-full -inset-px bg-blue-500 group-hover:shadow-lg group-hover:shadow-[#324A5F]"></div>
                         <Link to='/inputinfo' title="" class="relative inline-flex items-center justify-center px-8 py-3 text-base font-normal text-white bg-black border border-transparent rounded-full" role="button"> Get Started With Music </Link>
                     </div>
 
